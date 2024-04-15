84d158951276b1aa36a22dc244fc81bbc2e2b121
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2024-04-15 19:09:55 +0100

    index route added

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to Jobs API</h1><a href="/api/docs">Documentation</a>`);
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
